24ac10458a4380cb5379b5142d5b78ccfc6f9736
bhanutejgitlab <bhanutej.kalla89@gmail.com>
2023-03-27 23:27:19 +0530

    removed unneccessary method from repository

--- src/main/java/com/fleetenable/billing/repositories/AccountAccessorialParamRepository.java
+++ src/main/java/com/fleetenable/billing/repositories/AccountAccessorialParamRepository.java
@@ -11,7 +11,4 @@ public interface AccountAccessorialParamRepository extends MongoRepository<Accou
   
   @Query("{'account_accessorial_id': ?0, 'account_id': ?1}")
   AccountAccessorialParam findFirstByAccountAccessorialIdAndAccountId(String account_accessorial_id, String account_id);
-
-  @Query("{'account_accessorial_id': ?0, 'account_id': ?1, 'code': ?2, params_with_wt: ?3}")
-  AccountAccessorialParam findFirstByAccountAccessorialIdAndAccountIdAndCodeAndParamsWithWt(String account_accessorial_id, String account_id, String code, String params_with_wt);
 }
